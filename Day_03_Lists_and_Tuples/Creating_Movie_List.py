8a9db42a2c33bcc07c4695833b1c0e54eab8f8c7
Devender <2327uiet.devender6052@kuk.ac.in>
2026-02-15 14:10:07 +0530

    Update and rename createMovieList.py to Day_03_Lists_and_Tuples/Creating_Movie_List.py

--- Day_03_Lists_and_Tuples/Creating_Movie_List.py
+++ Day_03_Lists_and_Tuples/Creating_Movie_List.py
@@ -1,7 +1,9 @@
 movie=[]
 mov1=input("Enter the First Movie :")
+# insertion or adding the movie names in the list
 movie.append(mov1)
 movie.append(input("Enter the Second Movie :"))
 movie.append(input("Enter the Third Movie :"))
+
 
 print(movie)
