4e9b2465e374744d6b629d05d96867713ad6838a
Devender <2327uiet.devender6052@kuk.ac.in>
2026-02-17 23:13:00 +0530

    Practiced file handling concepts using Python

---
+++ Day_07_File_Handeling/Delete_File.py
@@ -0,0 +1,7 @@
+# using the os Module
+
+# Module (like a code library) is file written by another programmer that generally has a functions we can use
+
+import os
+# os.remove("filename")
+os.remove("Day 7(File handeling)\delete demo.txt")
---
+++ Day_07_File_Handeling/With_Syntax.py
@@ -0,0 +1,3 @@
+with open("Day 7(File handeling)\demo.txt","r") as f:
+    data=f.read()
+    print(data)
